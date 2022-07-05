cdddc808c3828884ad51915d00bf88232d0a4611
Hammad Asghar <ihammadasghar@gmail.com>
2022-07-05 17:32:19 +0100

    Generic relationships

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,19 @@
-from tkinter import CASCADE
 from django.db import models
 from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
+
+
+class Comment(models.Model):
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    
+    #  Generic relationship
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)  #  The type of model
+    object_id = models.PositiveIntegerField()  #  the id of the object of the model
+    content_object = GenericForeignKey("content_type", "object_id")  #  now it can be related to any type of model dynamicaly
 
 
 class Tag(models.Model):
@@ -20,6 +33,7 @@ class Post(models.Model):
     summary = models.TextField(max_length=500)
     content = models.TextField()
     tags = models.ManyToManyField(Tag, related_name="posts")
+    comments = GenericRelation(Comment)
 
     def __str__(self):
         return self.title

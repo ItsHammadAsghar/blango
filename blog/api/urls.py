62246d11c17ba97b788935b6b0326e9376664b38
Hammad Asghar <ihammadasghar@gmail.com>
2022-08-04 13:06:50 +0100

    View sets and routers

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,12 +1,16 @@
 from rest_framework.urlpatterns import format_suffix_patterns
 from django.urls import path, include, re_path
+from rest_framework.routers import DefaultRouter
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 import os
 
-from blog.api.views import PostList, PostDetail, UserDetail
+from blog.api.views import PostViewSet, UserDetail, TagViewSet
 from rest_framework.authtoken import views
 
+router = DefaultRouter()
+router.register("tags", TagViewSet)
+router.register("posts", PostViewSet)
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -19,8 +23,6 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path("posts/", PostList.as_view(), name="api_post_list"),
-    path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
     path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
 ]
 
@@ -28,7 +30,8 @@ urlpatterns = format_suffix_patterns(urlpatterns)
 
 urlpatterns += [
     path("auth/", include("rest_framework.urls")),
-    path("token-auth/", views.obtain_auth_token)
+    path("token-auth/", views.obtain_auth_token),
+    path("", include(router.urls)),
 ]
 
 urlpatterns += [
--- blog/api/views.py
+++ blog/api/views.py
@@ -1,44 +1,69 @@
 from rest_framework import mixins
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from blango_auth.models import User
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-from blog.models import Post
-from blog.api.serializers import PostSerializer, UserSerializer, PostDetailSerializer
+from blog.models import Post, Tag
+from blog.api.serializers import PostSerializer, UserSerializer, PostDetailSerializer, TagSerializer
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
 
 
-class PostList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class PostDetail(
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
-    generics.GenericAPIView,
-):
+class PostViewSet(viewsets.ModelViewSet):
     permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     queryset = Post.objects.all()
-    serializer_class = PostDetailSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
+    def get_serializer_class(self):
+        if self.action in ("list", "create"):
+            return PostSerializer
+        return PostDetailSerializer
 
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
 
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
-    
 class UserDetail(generics.RetrieveAPIView):
     lookup_field = "email"
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+    @action(methods=["get"], detail=True, name="Posts with the Tag")
+    def posts(self, request, pk=None):
+        tag = self.get_object()
+        post_serializer = PostSerializer(
+            tag.posts, many=True, context={"request": request}
+        )
+        return Response(post_serializer.data)
+
+
+# class PostList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+
+
+# class PostDetail(
+#     mixins.RetrieveModelMixin,
+#     mixins.UpdateModelMixin,
+#     mixins.DestroyModelMixin,
+#     generics.GenericAPIView,
+# ):
+#     permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
+#     queryset = Post.objects.all()
+#     serializer_class = PostDetailSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.retrieve(request, *args, **kwargs)
+
+#     def put(self, request, *args, **kwargs):
+#         return self.update(request, *args, **kwargs)
+
+#     def delete(self, request, *args, **kwargs):
+#         return self.destroy(request, *args, **kwargs)
